172afbd17f28ab545db6a70fa3fd9b274a17e3f3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-22 22:44:37 +0900

    セグエで移動する前にデータを受け渡す実装

--- WKWebView/WKWebView/WebListTableViewController.swift
+++ WKWebView/WKWebView/WebListTableViewController.swift
@@ -23,13 +23,11 @@ class WebListTableViewController: UITableViewController {
     
     // セクションの個数
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     // セクション内の行数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         // 配列webListの値の個数
         return webList.count
     }
@@ -61,12 +59,6 @@ class WebListTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-
-
-
-
 
     /*
     // Override to support conditional editing of the table view.
@@ -103,14 +95,20 @@ class WebListTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // セグエで移動する前にデータを受け渡す
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        // セグエがshowWebPageのときの処理
+        if segue.identifier == "showWebPage" {
+            // タップした行番号を取り出す
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                // 行のデータを取り出す
+                let webData = webList[(indexPath as NSIndexPath).row]
+                // 移動先のビューコントローラのdataプロパティに値を設定する
+                (segue.destination as! ViewController).data = webData
+            }
+        }
     }
-    */
 
 }
